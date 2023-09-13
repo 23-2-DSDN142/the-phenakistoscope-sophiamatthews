dca739e0e5c39e61cbc609e18a73f2836cda28d7
sophiamatthews <140354061+sophiamatthews@users.noreply.github.com>
2023-09-13 19:56:15 +1200

    heart
    made the shape of a heart

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -12,25 +12,39 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(heart);
+  layer1.mode( SWIRL(4) );
+  layer1.set_boundary( 300, 100 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function heart(x, y, animation, pScope){
   
   scale(animation.frame*2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  //left half of heart
+beginShape();
+fill(255, 0, 0);
+curveVertex(width/1, height*2);
+curveVertex(width/1, height*.9);
+curveVertex(width*.1, height/2);
+curveVertex(width/4, height/4);
+curveVertex(width/2, height/3);
+curveVertex(width/2, height);
+endShape();
 
+//right half of heart
+beginShape();
+curveVertex(width/2, height);
+curveVertex(width/2, height/3);
+curveVertex(width*.70, height/4);
+curveVertex(width*.9, height/2);
+curveVertex(width/2, height*.9);
+curveVertex(width/2, height*2);
+endShape();
 }
 
 function squares(x, y, animation, pScope){
@@ -40,10 +54,10 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  fill(0,0,0)
+  arc(x,y,400,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  fill(255,255,255)
+  rect(-10,-200-animation.wave()*50,30,100) // .wave is a cosine wave btw
 
 }
